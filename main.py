076bdfe9c42c89607d088feaa805e081836eed81
Patrick Glauner <patrick.oliver.glauner@gmail.com>
2016-06-23 17:32:55 +0200

    JMLR Proceedings

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from collections import Counter
 import itertools
 from jmlr import maybe_pickle_abstracts as get_jmlr_abstracts
+from jmlr_proc import maybe_pickle_abstracts as get_jmlr_proc_abstracts
 from general import get_keywords_of_single_abstract
 
 
@@ -13,10 +14,11 @@ sys.setdefaultencoding('utf8')
 
 def main():
     # TODO: better cleaning: stemming, get rid of \n, math, non-ASCII
-    jmlr_abstracts = get_jmlr_abstracts()
-    jmlr_keywords = [get_keywords_of_single_abstract(abs) for abs in jmlr_abstracts]
+    jmlr_keywords = [get_keywords_of_single_abstract(abs) for abs in get_jmlr_abstracts()]
+    jmlr_proc_keywords = [get_keywords_of_single_abstract(abs) for abs in get_jmlr_proc_abstracts()]
 
     keywords = list(itertools.chain(*jmlr_keywords))
+    keywords += list(itertools.chain(*jmlr_proc_keywords))
     all_keywords = Counter(keywords)
     for k in Counter(all_keywords).most_common(30):
         print k
