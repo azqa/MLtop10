c47eed8d21baae9cb5543e61ad468808256fcf45
Patrick Glauner <patrick.oliver.glauner@gmail.com>
2016-06-27 23:16:11 +0200

    Better main file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import itertools
 import jmlr
 import jmlr_proc
 import springer_ai
+import IEEE_TPAMI
 from general import get_keywords_of_single_abstract
 
 
@@ -15,22 +16,25 @@ sys.setdefaultencoding('utf8')
 
 def main():
     # TODO: better cleaning: get rid of \n, math, non-ASCII
-    jmlr_keywords = [get_keywords_of_single_abstract(abs) for abs in jmlr.maybe_pickle_abstracts()]
-    jmlr_proc_keywords = [get_keywords_of_single_abstract(abs) for abs in jmlr_proc.maybe_pickle_abstracts()]
-    springer_ai_keywords = [get_keywords_of_single_abstract(abs) for abs in springer_ai.maybe_pickle_springer_ai_raw_abstracts()]
+    source = dict()
+    source['jmlr'] = [get_keywords_of_single_abstract(abs) for abs in jmlr.maybe_pickle_abstracts()]
+    source['jmlr_proc'] = [get_keywords_of_single_abstract(abs) for abs in jmlr_proc.maybe_pickle_abstracts()]
+    source['springer_ai'] = [get_keywords_of_single_abstract(abs) for abs in springer_ai.maybe_pickle_springer_ai_raw_abstracts()]
+    source['IEEE_TPAMI'] = [get_keywords_of_single_abstract(abs) for abs in IEEE_TPAMI.maybe_pickle_abstracts()]
 
-    print 'Number of JMLR abstracts: {0}'.format(len(jmlr_keywords))
-    print 'Number of JMLR Proceedings abstracts: {0}'.format(len(jmlr_proc_keywords))
-    print 'Number of Springer AI abstracts: {0}'.format(len(springer_ai_keywords))
-    print 'Total = {0}'.format(len(jmlr_keywords) + len(jmlr_proc_keywords) + len(springer_ai_keywords))
+    print 'Number of JMLR abstracts: {0}'.format(len(source['jmlr']))
+    print 'Number of JMLR Proceedings abstracts: {0}'.format(len(source['jmlr_proc']))
+    print 'Number of Springer AI abstracts: {0}'.format(len(source['springer_ai']))
+    print 'Number of IEEE TPAMI abstracts: {0}'.format(len(source['IEEE_TPAMI']))
+    print 'Total = {0}'.format(sum([len(s) for s in source.values()]))
 
-    keywords = list(itertools.chain(*jmlr_keywords))
-    keywords += list(itertools.chain(*jmlr_proc_keywords))
-    keywords += list(itertools.chain(*springer_ai_keywords))
+    keywords = list(itertools.chain(*source['jmlr']))
+    keywords += list(itertools.chain(*source['jmlr_proc']))
+    keywords += list(itertools.chain(*source['springer_ai']))
+    keywords += list(itertools.chain(*source['IEEE_TPAMI']))
     all_keywords = Counter(keywords)
     for k in Counter(all_keywords).most_common(30):
         print k
 
 if __name__ == '__main__':
     main()
-
