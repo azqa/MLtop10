4f976765ca0bf3495a3b5bf24511373ab8aab987
Patrick Glauner <patrick.oliver.glauner@gmail.com>
2016-06-27 14:24:02 +0200

    Printing number of abstracts

--- main.py
+++ main.py
@@ -14,10 +14,15 @@ sys.setdefaultencoding('utf8')
 
 
 def main():
-    # TODO: better cleaning: stemming, get rid of \n, math, non-ASCII
+    # TODO: better cleaning: get rid of \n, math, non-ASCII
     jmlr_keywords = [get_keywords_of_single_abstract(abs) for abs in jmlr.maybe_pickle_abstracts()]
     jmlr_proc_keywords = [get_keywords_of_single_abstract(abs) for abs in jmlr_proc.maybe_pickle_abstracts()]
     springer_ai_keywords = [get_keywords_of_single_abstract(abs) for abs in springer_ai.maybe_pickle_springer_ai_raw_abstracts()]
+
+    print 'Number of JMLR abstracts: {0}'.format(len(jmlr_keywords))
+    print 'Number of JMLR Proceedings abstracts: {0}'.format(len(jmlr_proc_keywords))
+    print 'Number of Springer AI abstracts: {0}'.format(len(springer_ai_keywords))
+    print 'Total = {0}'.format(len(jmlr_keywords) + len(jmlr_proc_keywords) + len(springer_ai_keywords))
 
     keywords = list(itertools.chain(*jmlr_keywords))
     keywords += list(itertools.chain(*jmlr_proc_keywords))
