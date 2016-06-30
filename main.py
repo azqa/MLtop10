9858f23bab8f341863bb8ac37ca15d9eb0855f3c
Patrick Glauner <patrick.oliver.glauner@gmail.com>
2016-06-30 13:37:05 +0200

    New impact factors for weighting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from collections import Counter
 import itertools
+from operator import add
 import jmlr
 import jmlr_proc
 import springer_ai
@@ -15,34 +16,34 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-NAME = dict()
-NAME['jmlr'] = 'Journal of Machine Learning Research'
-NAME['jmlr_proc'] = 'Journal of Machine Learning Research Conference Proceedings'
-NAME['springer_ai'] = 'Springer Machine Learning'
-NAME['IEEE_TPAMI'] = 'IEEE Transactions on Pattern Analysis and Machine Intelligence'
-NAME['IEEE_NN'] = 'IEEE Transactions on Neural Networks'
-NAME['IEEE_NN_LS'] = 'IEEE Transactions on Neural Networks and Learning Systems'
-NAME['IEEE_KDA'] = 'IEEE Transactions on Knowledge and Data Engineering'
-NAME['IEEE_MI'] = 'IEEE Transactions on Medical Imaging'
-NAME['IEEE_EC'] = 'IEEE Transactions on Evolutionary Computation'
-NAME['IEEE_CIM'] = 'IEEE Computational Intelligence Magazine'
-NAME['IEEE_ASLP'] = 'IEEE Transactions on Audio, Speech, and Language Processing'
-NAME['IEEE_IS'] = 'IEEE Intelligent Systems'
-NAME['IEEE_SMCB'] = 'IEEE Transactions on Systems, Man, and Cybernetics, Part B (Cybernetics)'
-NAME['IEEE_FS'] = 'IEEE Transactions on Fuzzy Systems'
-NAME['IEEE_CVPR'] = 'Conference on Computer Vision and Pattern Recognition'
-NAME['IEEE_ICCV'] = 'International Conference on Computer Vision'
-NAME['Elsevier_KBS'] = 'Knowledge-Based Systems'
-NAME['Elsevier_NN'] = 'Neural Networks'
-NAME['Elsevier_Neuro'] = 'Neurocomputing'
-NAME['Elsevier_PR'] = 'Pattern Recognition'
-NAME['Elsevier_AI'] = 'Artificial Intelligence'
-NAME['Elsevier_CSL'] = 'Computer Speech and Language'
-NAME['Elsevier_PRL'] = 'Pattern Recognition Letters'
-NAME['Elsevier_CSDA'] = 'Computational Statistics & Data Analysis'
-NAME['Elsevier_IPM'] = 'Information Processing & Management'
-NAME['Elsevier_IPM'] = 'Data & Knowledge Engineering'
-NAME['Elsevier_CSR'] = 'Cognitive Systems Research'
+DETAILS = dict()
+DETAILS['jmlr'] = ('Journal of Machine Learning Research', 2.473)
+DETAILS['jmlr_proc'] = ('Journal of Machine Learning Research Conference Proceedings', None)
+DETAILS['springer_ai'] = ('Springer Machine Learning', 1.889)
+DETAILS['IEEE_TPAMI'] = ('IEEE Transactions on Pattern Analysis and Machine Intelligence', 5.781)
+DETAILS['IEEE_NN'] = ('IEEE Transactions on Neural Networks', 2.633)
+DETAILS['IEEE_NN_LS'] = ('IEEE Transactions on Neural Networks and Learning Systems', 4.291)
+DETAILS['IEEE_KDA'] = ('IEEE Transactions on Knowledge and Data Engineering', 2.067)
+DETAILS['IEEE_MI'] = ('IEEE Transactions on Medical Imaging', 3.390)
+DETAILS['IEEE_EC'] = ('IEEE Transactions on Evolutionary Computation', 3.654)
+DETAILS['IEEE_CIM'] = ('IEEE Computational Intelligence Magazine', 2.571)
+DETAILS['IEEE_ASLP'] = ('IEEE Transactions on Audio, Speech and Language Processing', 2.475)
+DETAILS['IEEE_IS'] = ('IEEE Intelligent Systems', 2.340)
+DETAILS['IEEE_SMCB'] = ('IEEE Transactions on Systems, Man, and Cybernetics, Part B (Cybernetics)', 6.220)
+DETAILS['IEEE_FS'] = ('IEEE Transactions on Fuzzy Systems', 8.746)
+DETAILS['IEEE_CVPR'] = ('Conference on Computer Vision and Pattern Recognition', None)
+DETAILS['IEEE_ICCV'] = ('International Conference on Computer Vision', None)
+DETAILS['Elsevier_KBS'] = ('Knowledge-Based Systems', 2.947)
+DETAILS['Elsevier_NN'] = ('Neural Networks', 2.708)
+DETAILS['Elsevier_Neuro'] = ('Neurocomputing', 2.083)
+DETAILS['Elsevier_PR'] = ('Pattern Recognition', 1.551)
+DETAILS['Elsevier_AI'] = ('Artificial Intelligence', 3.371)
+DETAILS['Elsevier_CSL'] = ('Computer Speech and Language', 1.753)
+DETAILS['Elsevier_PRL'] = ('Pattern Recognition Letters', 1.551)
+DETAILS['Elsevier_CSDA'] = ('Computational Statistics & Data Analysis', 1.400)
+DETAILS['Elsevier_IPM'] = ('Information Processing & Management', 1.265)
+DETAILS['Elsevier_IPM'] = ('Data & Knowledge Engineering', 1.115)
+DETAILS['Elsevier_CSR'] = ('Cognitive Systems Research', 0.831)
 
 
 def get_content(func, argument=None):
@@ -77,13 +78,23 @@ def main():
     for s in source.keys():
         count = len(source[s])
         total += count
-        print 'Number of {0} abstracts: {1}'.format(NAME[s], count)
+        print 'Number of {0} abstracts: {1}'.format(DETAILS[s][0], count)
 
     print 'Total = {0}'.format(total)
 
-    keywords = list(itertools.chain(*[list(itertools.chain(*source[s])) for s in source]))
-    all_keywords = Counter(keywords)
-    for k in Counter(all_keywords).most_common(200):
+    # Weight sources by impact factor
+    all_impacts = [d[1] for d in DETAILS.values() if d[1] != None]
+    mean_impact = 1. * sum(all_impacts) / len(all_impacts)
+    counters = dict()
+    for s in source.keys():
+        counters[s] = Counter(list(itertools.chain(*source[s])))
+        impact = DETAILS[s][1]
+        if not impact:
+            impact = mean_impact
+        for w in counters[s]:
+            counters[s][w] *= impact
+
+    for k in reduce(add, counters.values(), Counter()).most_common(200):
         print k
 
 if __name__ == '__main__':
