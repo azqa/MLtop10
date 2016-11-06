36d8d27f17d9ae7df4bdb36682f5b6db76eacd73
Patrick Glauner <patrick.oliver.glauner@gmail.com>
2016-11-06 10:08:53 +0100

    Better keyword extraction

--- general.py
+++ general.py
@@ -13,10 +13,18 @@ def get_keywords_of_single_abstract_grams(abstract):
     abstract = abstract.lower()
     tokenizer = RegexpTokenizer(r'\w+')
     tokens = tokenizer.tokenize(abstract)
-    tokens = [t for t in tokens if t not in STOPWORDS]
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(t) for t in tokens if t not in STOPWORDS]
     bigrams = nltk.bigrams(tokens)
     trigrams = nltk.trigrams(tokens)
-    return list(tokens) + list([' '.join(x) for x in bigrams]) + list([' '.join(x) for x in trigrams])
+    keyword_candidates = nltk.FreqDist(bigrams) + nltk.FreqDist(trigrams)
+
+    keywords = keyword_candidates.iteritems()
+    keywords = [(' '.join(w for w in k), score) for k, score in keywords]
+
+    sorted_keywords = sorted(keywords, key=operator.itemgetter(1), reverse=True)
+    total_keywords = len(sorted_keywords)
+    return [k[0] for k in sorted_keywords[0:total_keywords / 3]]
 
 
 def get_keywords_of_single_abstract_RAKE(abstract):
